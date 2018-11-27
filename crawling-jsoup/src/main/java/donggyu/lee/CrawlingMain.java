f75335ef0071a5ea5b18c4263f77cd40500b5f99
donggyuu <donggyu.rhee@gmail.com>
2018-11-27 20:58:06 +0900

    can get elements using 'select'

--- crawling-jsoup/src/main/java/donggyu/lee/CrawlingMain.java
+++ crawling-jsoup/src/main/java/donggyu/lee/CrawlingMain.java
@@ -13,11 +13,19 @@ import org.jsoup.select.Elements;
 /**
  * get title of notice from site by using Jsoup library (crawling)
  * @author donggyu.lee
- *
+ * 
+ * 参考↓
+ * https://jsoup.org/cookbook/extracting-data/attributes-text-html
+ * https://jsoup.org/cookbook/extracting-data/dom-navigation
+ * https://jsoup.org/apidocs/org/jsoup/select/Selector.html
+ * https://stackoverflow.com/questions/21336845/how-to-parse-ul-li-tags-using-jsoup-in-android
+ * 
  */
+
+// TODO : divide class - main class and logic class
 public class CrawlingMain {
 
-    // TODO : devide class - main class and logic class
+    // TODO : divide class - main class and logic class
     // TODO : below line is needed? what for?
     private final static String USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.152 Safari/537.36";
 
@@ -34,33 +42,46 @@ public class CrawlingMain {
                     .method(Connection.Method.GET)
                     .ignoreContentType(true);
 
+            System.out.println("after Jsoup.connect -> " + conn);
+
             Document doc = conn.get();
 
-            // get all HTML document
-            System.out.println(doc.toString());
-            System.out.println("=========================================");
+            // System.out.println("get all HTML as String -> " + doc.toString());
+            System.out.println("after conn.get() -> " + doc);
+            System.out.println(("get title elements only ->" + doc.title().toString()));
+            System.out.println("get all notice -> " + doc.getElementsByClass("list_title ellipsis"));
+            System.out.println("get all titles as one String -> " + doc.getElementsByClass("list_title ellipsis").text());
 
-            // get title element only
-            System.out.println((doc.title().toString()));
-
-            // get all notice
-            System.out.println(doc.getElementsByClass("list_title ellipsis"));
-            // get notice titles
-            System.out.println(doc.getElementsByClass("list_title ellipsis").text());
-
-            // get latest notice titles 
+            // get all titles as List<String>
             Elements resultElement = doc.getElementsByClass("list_title ellipsis");
-            System.out.println(elementToString(resultElement).get(0));
+            for(String webContent : elementToString(resultElement)) {
+                System.out.println(webContent);
+            }
 
-            // get all notice titles
-            elementToString(resultElement).forEach(System.out::println);
+            // get elements under <div class="list_area2">
+            Elements resultLinks = doc.select("div.list_area2 > *");
+            System.out.println("get elements under <div class=\"list_area2\">");
+            System.out.println(resultLinks);
+            // System.out.println(elementToString(resultLinks));
 
-            // elementToString(resultElement).get(2;
+            // need to close all spans for getting <li> or something under <div> or other class
+            // 参考 → https://stackoverflow.com/questions/21336845/how-to-parse-ul-li-tags-using-jsoup-in-android
+            // like below...
+            Elements ol = doc.select("div.list_area2 > ol");
+            Elements li = ol.select("li > *");
+            // System.out.println(li);
+
+            System.out.println("=================get absolute URLs only=================");
+            for (int i = 0; i < li.size(); i++) {
+                System.out.println(li.get(i).attr("abs:href"));
+            }
+
 
         } catch (IOException e){
             e.printStackTrace();
         }
     }
+
 
     // TODO remove "static"
     // change elements to String list
