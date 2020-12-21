8b6ad09f20c40bf23f9c81337d0240376f1ff8bd
donggyuu <donggyu.rhee@gmail.com>
2020-12-21 22:58:30 +0900

    ADD class for board form and entity

---
+++ restapi/src/main/java/com/example/restapi/entity/Board.java
@@ -0,0 +1,30 @@
+package com.example.restapi.entity;
+
+import com.example.restapi.form.CreateBoardForm;
+
+import java.time.LocalDateTime;
+
+public class Board {
+
+    private Long sequence;
+
+    private String userName;
+
+    private String content;
+
+    private LocalDateTime createdTime;
+
+    // default constructor
+    public Board(String userName, String content) {
+        this.userName = userName;
+        this.content = content;
+        this.createdTime = LocalDateTime.now();
+    }
+
+    // by user input form
+    public Board(CreateBoardForm form) {
+        this.userName = form.getUserName();
+        this.content = form.getContent();
+    }
+
+}
---
+++ restapi/src/main/java/com/example/restapi/form/CreateBoardForm.java
@@ -0,0 +1,12 @@
+package com.example.restapi.form;
+
+import lombok.Getter;
+
+@Getter
+public class CreateBoardForm {
+
+    private String userName;
+
+    private String content;
+
+}
