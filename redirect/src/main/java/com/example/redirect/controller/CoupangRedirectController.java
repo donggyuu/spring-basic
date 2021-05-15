1908a6af8380ef2f4da21969a0a19a1a48c29c36
donggyuu <donggyu.rhee@gmail.com>
2021-05-15 19:21:53 +0900

    INITIAL COMMITS

---
+++ redirect/src/main/java/com/example/redirect/controller/CoupangRedirectController.java
@@ -0,0 +1,19 @@
+package com.example.redirect.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CoupangRedirectController {
+
+    public static void main(String[] args) {
+
+        System.out.print("test controller");
+
+
+    }
+
+
+
+
+
+}
